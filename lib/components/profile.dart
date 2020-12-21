517ea9f8a466dc88f36294316e35c443e5dc8a07
YuanJing <908492549@qq.com>
2020-12-21 23:52:28 +0800

    [add]个人简历页

--- lib/components/profile.dart
+++ lib/components/profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -13,6 +15,7 @@ class _ProfileState extends State<Profile> {
       child: Column(
         children: <Widget>[
           AvatarWidget(),
+          Divider(color: Colors.grey,),
           Container(
             margin: const EdgeInsets.all(16.0 ),
             padding: const EdgeInsets.all(16.0 ),
@@ -24,6 +27,7 @@ class _ProfileState extends State<Profile> {
                In a material design application, consider using a [FlatButton] instead, or
               if that isn't appropriate, at least using an [InkWell] instead of"""),
           ),
+          Divider(color: Colors.grey,),
           _buildTitle("Skills"),
           SizedBox(height: 10.0,),
           _buildSkillRow("Java",0.8),
@@ -31,8 +35,66 @@ class _ProfileState extends State<Profile> {
           _buildSkillRow("Flutter",0.6),
           SizedBox(height: 10.0,),
           _buildTitle("Experience"),
+          _buildExperienceRow("广州世纪龙信息网络有限责任公司","Wordpress Developer","2019-2020"),
+          _buildExperienceRow("成都职工投资集团","java Developer","2020-2020"),
+          _buildExperienceRow("成都油管家科技有限责任公司","java Developer","2020-2020"),
           _buildTitle("Education"),
-          _buildTitle("Socials")
+          _buildExperienceRow("四川农业大学","计算机科学与技术","2015-2019"),
+          _buildExperienceRow("仁寿职业教育中心","升学班","2013-2015"),
+          _buildTitle("CONTACT"),
+          ListTile(
+            leading: Padding(
+              padding: const EdgeInsets.only(top: 8.0,left: 20.0),
+              child: Icon(Icons.email),
+            ),
+            title: Text("yuanjing.zlantan@gmail.com"),
+          ),
+          ListTile(
+            leading: Padding(
+              padding: const EdgeInsets.only(top: 8.0,left: 20.0),
+              child: Icon(Icons.phone),
+            ),
+            title: Text("18180102594"),
+          ),
+
+          Row(
+            children: [
+              IconButton(color: Colors.indigo,icon: Icon(FontAwesomeIcons.facebookF), onPressed: (){
+                _launchURL("https://facebook.com/lohanidamodar");
+              }),
+              IconButton(color: Colors.indigo,icon: Icon(FontAwesomeIcons.tencentWeibo), onPressed: (){
+                _launchURL("https://www.baidu.com");
+              }),
+              IconButton(color: Colors.indigo,icon: Icon(FontAwesomeIcons.youtube), onPressed: (){
+                _launchURL("https://youtube.com/c/reactbits");
+              }),
+              IconButton(color: Colors.indigo,icon: Icon(FontAwesomeIcons.github), onPressed: (){
+                _launchURL("https://github.com/lohanidamodar");
+              }),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  ListTile _buildExperienceRow(String company,String position,String duration){
+    return ListTile(
+      leading: Icon(Icons.arrow_right),
+      title: Text(company,style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("$position（$duration）"),
         ],
       ),
     );
@@ -51,9 +113,12 @@ class _ProfileState extends State<Profile> {
     return Row(
       children: <Widget>[
         Expanded(
-          flex: 2,
-          child: Text(title.toUpperCase()),
+            flex: 2,
+            child: Text(title.toUpperCase(),
+                textAlign: TextAlign.right,
+            )
         ),
+
         SizedBox(width: 10.0,),
         Expanded(
           flex: 5,
@@ -81,7 +146,14 @@ class AvatarWidget extends StatelessWidget {
         SizedBox(width: 50.0,),
         Container(
           width: 80.0,
-          child: Image.asset("assets/images/4.jpg"),
+          height: 80.0,
+          child: CircleAvatar(
+            radius:  40,
+              backgroundColor: Colors.purple,
+              child: CircleAvatar(
+                  radius: 35.0,
+                  backgroundImage: AssetImage("assets/images/4.jpg")),
+          )
         ),
         Column(
           children: <Widget>[
